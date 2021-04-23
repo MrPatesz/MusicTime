f0b38ade06bc541a015638569e1b50d625d1b94a
MrPatesz <patrik.angyan@users.noreply.github.com>
2021-04-23 22:52:25 +0200

    Player be lookin like a juicer

--- MusicTime.Web/music-time/src/Components/MusicPlayerComponent.tsx
+++ MusicTime.Web/music-time/src/Components/MusicPlayerComponent.tsx
@@ -6,6 +6,7 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 function MusicPlayerComponent() {
   const [urlArray] = useState<string[]>([
     "https://www.youtube.com/watch?v=1givs65YWBg",
+    "https://soundcloud.com/jahseh-onfroy/the-fall",
     "https://www.youtube.com/watch?v=lZ6hku1lbaQ",
     "https://soundcloud.com/miliy_10/toxic",
   ]);
@@ -27,33 +28,51 @@ function MusicPlayerComponent() {
 
   return (
     <div
-      className="d-flex flex-row"
-      style={{ position: "absolute", bottom: "10px", right: "10px" }}
+      className="d-flex flex-column bg-dark"
+      style={{ position: "fixed", bottom: "0", right: "0" }}
     >
-      <div className="mt-auto">
-        <div className="d-flex flex-column">
-          <input
-            type="range"
-            min={0}
-            max={1}
-            step="any"
-            value={volume}
-            onChange={(e: React.FormEvent<HTMLInputElement>) => {
-              setVolume(parseFloat(e.currentTarget.value));
-            }}
-          />
-          <ButtonGroup
-            style={{ maxHeight: "3rem", backgroundColor: "#282c34" }}
-          >
+      <div className="d-flex flex-row m-2">
+        <ReactPlayer
+          url={urlArray[index]}
+          controls={true}
+          playing={playing}
+          onEnded={playNext}
+          width={expanded ? 640 : 320}
+          height={expanded ? 360 : 180}
+          volume={volume}
+        />
+
+        <input
+          className="vertical-slider ml-2"
+          type="range"
+          min={0}
+          max={1}
+          step="any"
+          value={volume}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => {
+            setVolume(parseFloat(e.currentTarget.value));
+          }}
+        />
+      </div>
+
+      <div className="ml-auto mb-2">
+
+          <ButtonGroup className="mr-2">
             <Button variant="outline-info" onClick={playPrevious}>
               Previous
             </Button>
+          </ButtonGroup>
+          <ButtonGroup className="mr-2">
             <Button variant="outline-info" onClick={() => setPlaying(!playing)}>
               {playing ? "Pause" : "Play"}
             </Button>
+          </ButtonGroup>
+          <ButtonGroup className="mr-2">
             <Button variant="outline-info" onClick={playNext}>
               Next
             </Button>
+          </ButtonGroup>
+          <ButtonGroup className="mr-3">
             <Button
               variant="outline-info"
               onClick={() => setExpanded(!expanded)}
@@ -61,18 +80,8 @@ function MusicPlayerComponent() {
               {expanded ? "Shrink" : "Expand"}
             </Button>
           </ButtonGroup>
-        </div>
-      </div>
 
-      <ReactPlayer
-        url={urlArray[index]}
-        controls={true}
-        playing={playing}
-        onEnded={playNext}
-        width={expanded ? 640 : 320}
-        height={expanded ? 360 : 180}
-        volume={volume}
-      />
+      </div>
     </div>
   );
 }
