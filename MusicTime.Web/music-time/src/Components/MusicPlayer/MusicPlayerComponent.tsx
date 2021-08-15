54a58b5adfca455bb8f36e66ce6758d7d8486d36
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2021-08-15 14:54:30 +0200

    Hiding Player doesn't stop music anymore

--- MusicTime.Web/music-time/src/Components/MusicPlayer/MusicPlayerComponent.tsx
+++ MusicTime.Web/music-time/src/Components/MusicPlayer/MusicPlayerComponent.tsx
@@ -14,12 +14,18 @@ function MusicPlayerComponent() {
   const index = useSelector((state: RootState) => state.queue.index);
   const dispatch = useDispatch();
 
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [volume, setVolume] = useState<number>(0.15);
   const [progress, setProgress] = useState<number>(0);
-  const [isSeeking, setIsSeeking] = useState<boolean>(false);
+  const [scale, setScale] = useState<number>(1.1);
 
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [isSeeking, setIsSeeking] = useState<boolean>(false);
   const [hidden, setHidden] = useState<boolean>(false);
+  const [showQueue, setShowQueue] = useState<boolean>(false);
+
+  const playerRef = useRef<LegacyRef<ReactPlayer>>(
+    null
+  ) as RefObject<ReactPlayer>;
 
   function playPrevFunction() {
     setProgress(0);
@@ -33,151 +39,142 @@ function MusicPlayerComponent() {
     setIsPlaying(true);
   }
 
-  const playerRef = useRef<LegacyRef<ReactPlayer>>(
-    null
-  ) as RefObject<ReactPlayer>;
-
-  const [showQueue, setShowQueue] = useState<boolean>(false);
-
   function onProgress(state: { played: number }) {
     if (!isSeeking) setProgress(state.played);
   }
 
-  const [scale, setScale] = useState<number>(1.1);
-
   return (
     <div style={{ position: "fixed", bottom: "0", right: "0" }}>
-      {!hidden ? (
-        <div className="d-flex flex-row bg-dark border border-info">
-          <QueueComponent show={showQueue}></QueueComponent>
+      <div
+        className={
+          hidden ? "d-none" : "d-flex flex-row bg-dark border border-info"
+        }
+      >
+        <QueueComponent show={showQueue}></QueueComponent>
 
-          <div className="d-flex flex-column">
-            <div className="d-flex flex-row m-2">
-              <ReactPlayer
-                ref={playerRef}
-                url={
-                  queue.length > 0 ? (queue[index] ? queue[index].url : "") : ""
-                }
-                playing={isPlaying}
-                width={scale * 320}
-                height={scale * 180}
-                volume={volume}
-                loop={false}
-                onEnded={playNextFunction}
-                onError={playNextFunction}
-                onPause={() => setIsPlaying(false)}
-                onPlay={() => setIsPlaying(true)}
-                onProgress={onProgress}
-                onStart={() => setIsPlaying(true)}
-              />
-            </div>
-
-            <input
-              className="mx-2 horizontal-slider"
-              type="range"
-              min={0}
-              max={0.999999}
-              step="any"
-              value={progress}
-              onMouseDown={() => setIsSeeking(true)}
-              onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                setProgress(parseFloat(e.currentTarget.value))
+        <div className="d-flex flex-column">
+          <div className="d-flex flex-row m-2">
+            <ReactPlayer
+              ref={playerRef}
+              url={
+                queue.length > 0 ? (queue[index] ? queue[index].url : "") : ""
               }
-              onMouseUp={(e: React.FormEvent<HTMLInputElement>) => {
-                setIsSeeking(false);
-                playerRef?.current?.seekTo(parseFloat(e.currentTarget.value));
-              }}
+              playing={isPlaying}
+              width={scale * 320}
+              height={scale * 180}
+              volume={volume}
+              loop={false}
+              onEnded={playNextFunction}
+              onError={playNextFunction}
+              onPause={() => setIsPlaying(false)}
+              onPlay={() => setIsPlaying(true)}
+              onProgress={onProgress}
+              onStart={() => setIsPlaying(true)}
             />
-
-            <div className="m-2 d-flex flex-row">
-              <ButtonGroup>
-                <Button
-                  variant={showQueue ? "info" : "outline-info"}
-                  onClick={() => setShowQueue(!showQueue)}
-                >
-                  <FontAwesomeIcon icon="list" />
-                </Button>
-              </ButtonGroup>
-
-              <ButtonGroup className="mx-auto">
-                <Button
-                  variant="outline-info"
-                  onClick={playPrevFunction}
-                  className="mr-2"
-                >
-                  <FontAwesomeIcon icon="step-backward" />
-                </Button>
-
-                <Button
-                  className="mr-2"
-                  variant="outline-info"
-                  onClick={() => setIsPlaying(!isPlaying)}
-                >
-                  {isPlaying ? (
-                    <FontAwesomeIcon icon="pause" />
-                  ) : (
-                    <FontAwesomeIcon icon="play" />
-                  )}
-                </Button>
-
-                <Button variant="outline-info" onClick={playNextFunction}>
-                  <FontAwesomeIcon icon="step-forward" />
-                </Button>
-              </ButtonGroup>
-
-              <ButtonGroup>
-                <Button
-                  variant="outline-warning"
-                  onClick={() => setHidden(true)}
-                >
-                  <FontAwesomeIcon icon="eye-slash" />
-                </Button>
-              </ButtonGroup>
-            </div>
           </div>
 
-          <div className="d-flex flex-row mt-auto mb-2 mr-2 text-info">
-            <div className="d-flex flex-column mr-2">
-              <FontAwesomeIcon icon="volume-up" />
-              <input
-                className="vertical-slider my-2"
-                type="range"
-                min={0}
-                max={1}
-                step="any"
-                value={volume}
-                onChange={(e: React.FormEvent<HTMLInputElement>) => {
-                  setVolume(parseFloat(e.currentTarget.value));
-                }}
-              />
-              <FontAwesomeIcon icon="volume-down" />
-            </div>
-            <div className="d-flex flex-column">
-              <FontAwesomeIcon icon="expand" />
-              <input
-                className="vertical-slider my-2"
-                type="range"
-                min={1.1}
-                max={4.7}
-                step="any"
-                value={scale}
-                onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                  setScale(parseFloat(e.currentTarget.value))
-                }
-              />
-              <FontAwesomeIcon icon="compress" />
-            </div>
+          <input
+            className="mx-2 horizontal-slider"
+            type="range"
+            min={0}
+            max={0.999999}
+            step="any"
+            value={progress}
+            onMouseDown={() => setIsSeeking(true)}
+            onChange={(e: React.FormEvent<HTMLInputElement>) =>
+              setProgress(parseFloat(e.currentTarget.value))
+            }
+            onMouseUp={(e: React.FormEvent<HTMLInputElement>) => {
+              setIsSeeking(false);
+              playerRef?.current?.seekTo(parseFloat(e.currentTarget.value));
+            }}
+          />
+
+          <div className="m-2 d-flex flex-row">
+            <ButtonGroup>
+              <Button
+                variant={showQueue ? "info" : "outline-info"}
+                onClick={() => setShowQueue(!showQueue)}
+              >
+                <FontAwesomeIcon icon="list" />
+              </Button>
+            </ButtonGroup>
+
+            <ButtonGroup className="mx-auto">
+              <Button
+                variant="outline-info"
+                onClick={playPrevFunction}
+                className="mr-2"
+              >
+                <FontAwesomeIcon icon="step-backward" />
+              </Button>
+
+              <Button
+                className="mr-2"
+                variant="outline-info"
+                onClick={() => setIsPlaying(!isPlaying)}
+              >
+                {isPlaying ? (
+                  <FontAwesomeIcon icon="pause" />
+                ) : (
+                  <FontAwesomeIcon icon="play" />
+                )}
+              </Button>
+
+              <Button variant="outline-info" onClick={playNextFunction}>
+                <FontAwesomeIcon icon="step-forward" />
+              </Button>
+            </ButtonGroup>
+
+            <ButtonGroup>
+              <Button variant="outline-warning" onClick={() => setHidden(true)}>
+                <FontAwesomeIcon icon="eye-slash" />
+              </Button>
+            </ButtonGroup>
           </div>
         </div>
-      ) : (
-        <Button
-          variant="outline-info"
-          onClick={() => setHidden(false)}
-          className="m-2"
-        >
-          <FontAwesomeIcon icon="eye" />
-        </Button>
-      )}
+
+        <div className="d-flex flex-row mt-auto mb-2 mr-2 text-info">
+          <div className="d-flex flex-column mr-2">
+            <FontAwesomeIcon icon="volume-up" />
+            <input
+              className="vertical-slider my-2"
+              type="range"
+              min={0}
+              max={1}
+              step="any"
+              value={volume}
+              onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                setVolume(parseFloat(e.currentTarget.value));
+              }}
+            />
+            <FontAwesomeIcon icon="volume-down" />
+          </div>
+          <div className="d-flex flex-column">
+            <FontAwesomeIcon icon="expand" />
+            <input
+              className="vertical-slider my-2"
+              type="range"
+              min={1.1}
+              max={4.7}
+              step="any"
+              value={scale}
+              onChange={(e: React.FormEvent<HTMLInputElement>) =>
+                setScale(parseFloat(e.currentTarget.value))
+              }
+            />
+            <FontAwesomeIcon icon="compress" />
+          </div>
+        </div>
+      </div>
+
+      <Button
+        variant="outline-info"
+        onClick={() => setHidden(false)}
+        className={hidden ? "m-2" : "d-none"}
+      >
+        <FontAwesomeIcon icon="eye" />
+      </Button>
     </div>
   );
 }
